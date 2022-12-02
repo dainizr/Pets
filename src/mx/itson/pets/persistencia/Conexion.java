85268756c588fd9baae32405f9cddcedd867e922
52622 <52622@LAPTOP-HUS3LIR3>
2022-12-01 20:56:56 -0700

    Se agregó un comentario del método que conecta a la base de datos

---
+++ src/mx/itson/pets/persistencia/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.pets.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Este método conecta con la base de datos veterinariadb por medio del conector.
+ *  la base de datos conectada
+ */
+
+public class Conexion {
+    
+    public static Connection obtener(){
+        Connection conexion =null;
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/veterinariadb?user=root&password=root");
+        } catch (Exception ex){
+            System.err.println("Ocurrió un error: " + ex.getMessage());
+        }
+        return conexion;
+    }
+    
+}
